c4d87d4dea0dac2f016dd25cf003294fe50ef043
kellie <kellie@wackywizards.org>
2026-05-22 22:25:55 +0300

    Add separate DialogueHistoryEntry panel, voiceline play button

---
+++ Libraries/VNBase/Code/UI/SubPanels/DialogueHistoryEntry.razor.cs
@@ -0,0 +1,19 @@
+﻿namespace VNBase.UI;
+
+public partial class DialogueHistoryEntry
+{
+	[Parameter]
+	public ScriptPlayer.HistoryEntry? Entry { get; set; }
+	
+	private void PlayDialogueVoiceline()
+	{
+		if ( Entry is null || Entry.Dialogue.Voiceline is null )
+		{
+			return;
+		}
+		
+		var voiceline = Entry.Dialogue.Voiceline;
+		voiceline.Stop();
+		voiceline.Play();
+	}
+}
